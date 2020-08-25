7af08f9e71cca48e673d48c8947b57f7ba57582a
Ekaterina Vashurina <wasturina.ekatterina@gmail.com>
2020-08-25 20:33:21 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'use stict'
+
+//alert("Hello");
+//let answer = confirm("Hey you!");
+//console.log(answer);
+//let arr = ['ef.jpg', 'cjgvh.png', 'vgh.svg'];
+
+
+
+//let answer = +prompt("Hey you!", "no");
+//console.log(typeof(answer));
+let incr = 10, decr = 10; 
+console.log(++incr, --decr)
